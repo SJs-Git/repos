4cc33b74b450cc24a66dc60b4aa6c8730aad7d6a
Sireesha Jangili <sirejan2023@gmail.com>
2025-07-19 10:39:20 +0530

    Added another script

---
+++ 01-conversation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hi Ani, how are you doing"
+echo "Hi Anu, I am doing good. Thank you! How are you doing"
+echo "I am doing good too, Anu. Thank you!"
+echo "Ani, Can I know which fruit you like the most?"
+echo "Mango is my favourite fruit, Anu."
+echo "Oh, great!!"
+
+
