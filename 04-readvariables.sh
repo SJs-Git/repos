f7b5e36c7bb5bb9aae8f0ccaf068e36d01d17d64
Sireesha Jangili <sirejan2023@gmail.com>
2025-07-19 11:15:28 +0530

    Using read option

---
+++ 04-readvariables.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# reading and displaying the variables upon entering values at runtime
+
+# PERSON1=$1
+# PERSON2=$2
+
+echo "Hi there, Please enter your name:"
+
+read NAME 
+echo "Hi  NAME How are you doing"
+echo "I am doing good too. Thank you!"
+
+
+echo "Can I know which fruit you like the most?"
+
+read FRUIT 
+
+echo "FRUIT is my favourite fruit."
+
+
+echo "Oh, great!!"
