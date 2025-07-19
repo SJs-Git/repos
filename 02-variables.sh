8bf0f680703aeb8b11382ab41e19fdad8483eb07
Sireesha Jangili <sirejan2023@gmail.com>
2025-07-19 10:55:59 +0530

    Using Variables in the Script

---
+++ 02-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Assigning values to variables
+
+PERSON1=ANI
+PERSON2=ANU
+
+echo "Hi $PERSON1, how are you doing"
+echo "Hi ${PERSON2}, I am doing good. Thank you! How are you doing"
+echo "I am doing good too, ${PERSON2}. Thank you!"
+echo "$PERSON1, Can I know which fruit you like the most?"
+echo "Mango is my favourite fruit, ${PERSON2}."
+echo "Oh, great!!"
